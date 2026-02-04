918aaf84068e02f997f3f53d8af612ecb2559ca6
Roméo <romeo.jacquemine@gmail.com>
2026-02-04 16:36:36 +0100

    mercredi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <iostream>
 #include <cstdlib>
 #include <sil/sil.hpp>
 #include <cmath>
+#include <complex>
 
 void clean_image() {
     sil::Image image{"images/photo.jpg"};
@@ -464,9 +466,9 @@ void glitch() {
         int size_y = rand() % 30 + 5;
 
         int x1 = rand() % (image.width() - size_x);
-        int y1 = rand() % (image.width() - size_y);
+        int y1 = rand() % (image.height() - size_y);
         int x2 = rand() % (image.width() - size_x);
-        int y2 = rand() % (image.width() - size_y);
+        int y2 = rand() % (image.height() - size_y);
 
         for (int x = 0; x < size_x; x++)
         {
@@ -481,7 +483,178 @@ void glitch() {
     image.save("output/pouet.png");
 }
 
+void pixel_sort() {
+    sil::Image image{"images/logo.png"};
+    int number = 300;
+    for (int i = 0; i < number; i++)
+    {
+        int size_x = rand() % 20 + 45 ;
+
+        int x_begin = rand() % (image.width() - size_x);
+        int y = rand() % (image.height());
+
+        std::vector<glm::vec3> v;
+        for (int x = x_begin; x < size_x + x_begin; x++)
+        {
+             glm::vec3& pixel = image.pixel(x, y);
+             v.push_back(pixel);
+        }
+        std::sort(v.begin(), v.end(),  [](glm::vec3 const& color1, glm::vec3 const& color2){
+            float moy = (color1.b + color1.g + color1.r) / 3;
+            float moy2 = (color2.b + color2.g + color2.r) / 3;
+            return moy < moy2;
+        });
+        for (int x = x_begin; x < size_x + x_begin; x++)
+        {
+             image.pixel(x, y) = v.at(x - x_begin);
+        }
+        
+    }
+    image.save("output/pouet.png");
+}
+
+void mandelbrot(){
+    sil::Image image{500, 500};
+    for (float i{0}; i < image.width(); i++)        
+    {
+        for (float j{0}; j < image.height(); j++)
+        {
+            float x = (i / 125) - 2;
+            float y = (j / 125) - 2;
+            std::complex<float> c{x, y};
+            std::complex<float> z = c;
+            float compteur = 0;
+            while (std::abs(z) < 2)
+            {
+                z = z * z + c;
+                if (compteur == 30)
+                {
+                    break;
+                }
+                compteur++;
+            }
+            glm::vec3& pixel = image.pixel(i, j);
+            pixel.r = compteur / 30;
+            pixel.g = compteur / 30;
+            pixel.b = compteur / 30;
+
+        }           
+    }
+    image.save("output/pouet.png");
+}
+
+glm::vec3 to_linear(glm::vec3 color) {
+    if (color.r <= 0.04045)
+    {
+        color.r = color.r / 12.92;
+    } else {
+        color.r = pow((color.r + 0.055) / 1.055, 2.4);
+    }
+    
+    if (color.g <= 0.04045)
+    {
+        color.g = color.g / 12.92;
+    } else {
+        color.g = pow((color.g + 0.055) / 1.055, 2.4);
+    }
+
+    if (color.b <= 0.04045)
+    {
+        color.b = color.b / 12.92;
+    } else {
+        color.b = pow((color.b + 0.055) / 1.055, 2.4);
+    }
+
+    return color;
+}
+
+
+glm::vec3 to_lab(glm::vec3 const& c) {
+    glm::vec3 lab_color;
+    // r est le L
+    lab_color.r = 0.4122214708f * c.r + 0.5363325363f * c.g + 0.0514459929f * c.b;
+    // g est le m
+    lab_color.g = 0.2119034982f * c.r + 0.6806995451f * c.g + 0.1073969566f * c.b;
+	// b est le s
+    lab_color.b = 0.0883024619f * c.r + 0.2817188376f * c.g + 0.6299787005f * c.b;
+
+    lab_color.r = cbrtf(lab_color.r);
+    lab_color.g = cbrtf(lab_color.g);
+    lab_color.b = cbrtf(lab_color.b);
+
+    return {
+        0.2104542553f*lab_color.r + 0.7936177850f*lab_color.g - 0.0040720468f*lab_color.b,
+        1.9779984951f*lab_color.r - 2.4285922050f*lab_color.g + 0.4505937099f*lab_color.b,
+        0.0259040371f*lab_color.r + 0.7827717662f*lab_color.g - 0.8086757660f*lab_color.b,
+    };
+}
+
+glm::vec3 oklab_to_linear_(glm::vec3 c) 
+{
+    float l_ = c.r + 0.3963377774f * c.g + 0.2158037573f * c.b;
+    float m_ = c.r - 0.1055613458f * c.g - 0.0638541728f * c.b;
+    float s_ = c.r - 0.0894841775f * c.g - 1.2914855480f * c.b;
+
+    float l = l_*l_*l_;
+    float m = m_*m_*m_;
+    float s = s_*s_*s_;
+
+    return {
+		+4.0767416621f * l - 3.3077115913f * m + 0.2309699292f * s,
+		-1.2684380046f * l + 2.6097574011f * m - 0.3413193965f * s,
+		-0.0041960863f * l - 0.7034186147f * m + 1.7076147010f * s,
+    };
+}
+
+glm::vec3 linear_to_rgb(glm::vec3 color){
+    if (color.r <= 0.0031308)
+    {
+        color.r = color.r * 12.92;
+    } else {
+        color.r = pow(color.r, 1/2.4) * 1.055 - 0.055;
+    }
+    
+    if (color.g <= 0.0031308)
+    {
+        color.g = color.g * 12.92;
+    } else {
+        color.g = pow(color.g, 1/2.4) * 1.055 - 0.055;
+    }
+
+    if (color.b <= 0.0031308)
+    {
+        color.b = color.b * 12.92;
+    } else {
+        color.b = pow(color.b, 1/2.4) * 1.055 - 0.055;
+    }
+
+    return color;
+}
+
+void color_buzz_cut() {
+    sil::Image image{"images/logo.png"};
+
+    glm::vec3 color1_lab = to_lab(to_linear(glm::vec3{1, 0, 0}));
+    glm::vec3 color2_lab = to_lab(to_linear(glm::vec3{0, 1, 0}));
+
+    for (float i{0}; i < image.width(); i++)
+    {
+        float percentage1 = (image.width() - i) /   image.width();
+        float percentage2 = i / image.width();
+        glm::vec3 new_lab_color = percentage1 * color1_lab + percentage2 * color2_lab;
+        glm::vec3 new_color = linear_to_rgb(oklab_to_linear_(new_lab_color));
+        for (float j{0}; j < image.height(); j++)
+        {
+            image.pixel(i, j) = new_color;
+        }
+        
+    }
+    
+    
+    image.save("output/pouet.png");
+}
+
 int main()
 {
-    polar_rosace();
+    mandelbrot();
 } 
